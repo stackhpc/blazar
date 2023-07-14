44bdbee1a0f457cab5ec87fcb31799077d17e900
John Garbutt <john.garbutt@stackhpc.com>
2023-07-14 14:58:17 +0100

    Get more info
    Change-Id: Ic1f1a32decbd9de46dda4ffd564ba1ceb7d4722f

--- blazar/plugins/instances/instance_plugin.py
+++ blazar/plugins/instances/instance_plugin.py
@@ -281,7 +281,7 @@ class VirtualInstancePlugin(base.BasePlugin, nova.NovaClientWrapper):
         if resource_properties:
             filters += plugins_utils.convert_requirements(resource_properties)
 
-        LOG.debug(f"Filters are: {filters}")
+        LOG.debug(f"Filters are: {filters} {cpus} {resource_inventory}")
         hosts = db_api.reservable_host_get_all_by_queries(filters)
 
         LOG.debug(f"Found some hosts from db: {hosts}")
